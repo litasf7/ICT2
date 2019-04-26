ac9d792e2f9e295b99a1166ef748e76a1a29d4c6
Jiona Joy <34472480+jionajoy@users.noreply.github.com>
2019-04-26 15:38:43 +1000

    Add files via upload
    new home and nav

---
+++ navbarJ.php
@@ -0,0 +1,37 @@
+
+
+<nav class="navbar navbar-expand-lg navbar navbar-#f4511e bg-#f4511e">
+  <a class="navbar-brand" href="index.php" style="color:#f4511e; font-size:24px" >Band Of Barbers</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="background-color:#f4511e" >
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0" >
+      <li class="nav-item " >
+        <a class="nav-link" href="index.php" style="color:#ffffff">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link" href="salon.php" style="color:#ffffff">Salons</a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link" href="about.php" style="color:#ffffff">Why Us?</a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link" href="contact.php" style="color:#ffffff">Contact</a>
+      </li>
+      
+      <li class="nav-item" >
+        <a class="nav-link " href="login.php" style="color:#ffffff">Login</a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link " href="register1.php" style="color:#ffffff">Registration</a>
+      </li>
+    </ul>
+   
+    <form class="form-inline" >
+   <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" >
+   <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="color:#ffffff"  >Search</button>
+ </form>
+  </div>
+</nav>
