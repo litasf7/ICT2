7dbe3c07c40b7579507f505bbe1c320d3ecb3468
Jiona Joy <34472480+jionajoy@users.noreply.github.com>
2019-05-16 18:40:19 +1000

    Add files via upload

--- user_profile.php
+++ user_profile.php
@@ -1,41 +1,51 @@
-<?php 
+
+<?php
 session_start();
-include("nav.php");
-include "include/config.php";
-?>
-   
-<?php 
+$user_check = $_SESSION['email'];
+include("config.php");
+		//$connection = new mysqli("localhost", "root", "root", "ictatjcu_bandofbarbers");
+		$user_sql = "select * from user where email='$user_check'";
+   		$user_sql = $connection -> query($user_sql);
+
+		if ($user_sql -> num_rows == 1) {
+    	//make a query to check if a valid user
+    	$ses_sql = "select * from user where email='$user_check'";
+
   	// check session variable
   	if (isset($_SESSION['email']))
   	{
-		//make the database connection
-		$connection = new mysqli("localhost", "root", "root", "ictatjcu_bandofbarbers");
+
 		$user_check = $_SESSION['email'];
-   
+
+		//$connection = new mysqli("localhost", "root", "root", "ictatjcu_bandofbarbers");
+
+
     	//make a query to check if a valid user
     	$ses_sql = "select * from user where email='$user_check'";
     	$result = $connection -> query($ses_sql);
-        
+
 		if ($result -> num_rows == 1) {
 			$row = $result -> fetch_assoc();
 			$id = $row['user_id'];
 			$fname = $row['fname'];
 			$lname = $row['lname'];
 			$email = $row['email'];
-			
-    		
-		}
-			
-	}	?>
+			$saddress=$row['saddress'];
+			$city=$row['city'];
+			$pin=$row['pin'];
+			$state=$row['state'];
+			$number=$row['number'];
 
+	?>
+<?php include("nav.php"); ?>
 
 <div class="container" style="text-align:center; ">
-    <h1 class="text-info" style="margin-top:40px">My Account</h1>
-    <a  href=user_update.php class="btn btn-outline-info" style="float:right" >EDIT</a><br><hr>
-    
-	  <table class="table table-info " style="width:50%; margin-left:25%; margin-top:50px">
-         
-          
+    <h1 class="text-danger" style="margin-top:40px">My Account</h1>
+    <a  href=user_update.php class="btn btn-outline-danger" style="float:right" >EDIT</a><br><hr>
+
+	  <table class="table table-danger " style="width:50%; margin-left:25%; margin-top:50px">
+
+
           <tr >
              <td> First Name:</td>
             <td><?php echo $row["fname"] ?></td>
@@ -48,12 +58,38 @@ include "include/config.php";
               <td>Email:</td>
                 <td><?php echo $row["email"] ?></td>
           </tr>
+					<tr>
+						<td>Address:</td>
+						 <td><?php echo $row['saddress']; ?></td>
+					</tr>
+					<tr>
+						<td>City:</td>
+						<td><?php echo $row['city']; ?></td>
+					</tr>
+					<tr>
+						<td>Pin:</td>
+						<td><?php echo $row['pin']; ?></td>
+					</tr>
+					<tr>
+						<td>State:</td>
+						<td><?php echo $row['state']; ?></td>
+					</tr>
+					<tr>
+						<td>Phone:</td>
+						 <td><?php echo $row['number']; ?></td>
+					</tr>
     </table></div> <br><br><br><br><br><br><br><br><br>
 		<!-- Footer -->
- <div class="footer">
-<div class="jumbotron text-center bg-info" style="margin-bottom:0; padding-top:40px; padding-bottom:10px;">
-  
-<?php include("footer.php")?>
-  
+<div class="footer">
+<div class=" text-light bg-dark" style="margin-bottom:0;  padding-bottom:10px; padding-top:20px;">
+
+<?php include("footer.php") ?>
+
      </div></div>
   <!-- Footer -->
+<?php } ?>
+  <?php } ?>
+  <?php } else {
+	header(("Location:index.php"));
+?>
+<?php }?>
