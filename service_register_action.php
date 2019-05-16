edac7c131a85e61d6c3a5114e34d5e2e349b82d9
Jiona Joy <34472480+jionajoy@users.noreply.github.com>
2019-05-16 15:56:08 +1000

    Add files via upload

--- service_register_action.php
+++ service_register_action.php
@@ -1,63 +1,56 @@
 <?php
 session_start();
-
 include("config.php");
+
+if(isset($_POST["register"])){
+	
+$user_check = $_SESSION['email'];
 if (isset($_POST["register"])) {
      //$connection = new mysqli("localhost", "root", "root", "ictatjcu_bandofbarbers");
 		 $image_path = 'uploads/'.$_FILES['image']['name'];
 		 $image_path = $connection->real_escape_string($image_path);
-		$serv_name = $connection->real_escape_string($_POST["serv_name"]);
-		$serv_desc = $connection->real_escape_string($_POST["serv_desc"]);
-		$sprice = $connection->real_escape_string($_POST["sprice"]);
+		$serv_name = $connection->real_escape_string($_POST["service"]);
+		$serv_desc = $connection->real_escape_string($_POST["description"]);
+		$sprice = $connection->real_escape_string($_POST["price"]);
 
 
 
-		$data1 = $connection->query("SELECT * FROM services WHERE serv_name= '$serv_name'");
+		$data1 = $connection->query("SELECT * FROM services WHERE service= '$serv_name'");
+        
 
 		if ($data1->num_rows>0)
 		{
-			$message = "You have already registered this Service";
-			echo "<script type='text/javascript'>alert('$message');
+			$message1 = "You have already registered this Service";
+			echo "<script type='text/javascript'>alert('$message1');
 			window.location.href='salon_serv.php';
 			</script>";
 
 		}
 		else
 		{
-				if(preg_match("!image!",$_FILES['image']['type'])){
-				if(copy($_FILES['image']['tmp_name'],$image_path)){
-
-
-		$data = $connection->query("INSERT INTO services (image, serv_name , serv_desc, sprice) VALUES ('$image_path', '$serv_name' , '$serv_desc', '$sprice')");
+				
+		$data = $connection->query("INSERT INTO services (image, service , description, price, salon_id) VALUES ('$image_path', '$serv_name' , '$serv_desc', '$sprice', (SELECT salon_id FROM salon WHERE email = '$user_check'))");
 
     	if ($data === false)
 		{
 			//echo "<img src='$image_path' />";
-			$message = "Error Please Try Again";
-			echo "<script type='text/javascript'>alert('$message');
+			$message2 = "Error Please Try Again";
+			echo "<script type='text/javascript'>alert('$message2');
 			window.location.href='salon_serv.php';
 			</script>";
 		}
 
     	else
 		{
-			$message = "Your sevice has been added";
-			echo "<script type='text/javascript'>alert('$message');
+			$message3 = "Your sevice has been added";
+			echo "<script type='text/javascript'>alert('$message3');
 			window.location.href='salon_welcome.php';
 			</script>";
 
 		  }
-	    }
-
-	  }
-		else{
-				$message = "Please Insert Your Salon Image";
-			echo "<script type='text/javascript'>alert('$message');
-			window.location.href='salon_serv.php';
-			</script>"	;
-				}
+	   
 		}
 
-}
+}}
 
 ?>
